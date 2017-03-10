1b138ea75a1e4a76a222e1fa88e1333954a1f383
tzuhsien <b03902080@ntu.edu.tw>
2017-03-10 20:13:39 +0800

    take -i and -o from lab1.sh

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -1,24 +1,46 @@
+#! /bin/bash
+#clean Firewall
 iptables -F
 iptables -X
 iptables -Z
+#clean NAT
+iptables -t nat -F
+iptables -t nat -X
+iptables -t nat -Z
+
+#Let NAT do translate form private ip to computerA's ip
 iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.100.10:80
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+#echo "1" > /proc/sys/net/ipv4/ip_forward
+
+#default
+iptables -P INPUT  DROP
 iptables -P FORWARD DROP
-iptables -A FORWARD -i eth1 -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -o eth1 -p tcp --sport 80 -j ACCEPT
-#iptables -A FORWARD -i eth1 -p udp --dport 80 -j ACCEPT
-#iptables -A FORWARD -o eth1 -p udp --sport 80 -j ACCEPT
-#iptables -A FORWARD -i eth1 -p tcp --dport 443 -j ACCEPT
-#iptables -A FORWARD -o eth1 -p tcp --sport 443 -j ACCEPT
-iptables -A FORWARD -i eth1 -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -o eth1 -p udp --sport 53 -j ACCEPT
-iptables -A FORWARD -i eth1 -p tcp --dport 22 -j ACCEPT
-iptables -A FORWARD -o eth1 -p tcp --sport 22 -j ACCEPT
-iptables -A FORWARD -i eth1 -p tcp --dport 21 -j ACCEPT
-iptables -A FORWARD -o eth1 -p tcp --sport 21 -j ACCEPT
-iptables -A FORWARD -i eth1 -p tcp --dport 20 -j ACCEPT
-iptables -A FORWARD -o eth1 -p tcp --sport 20 -j ACCEPT
-iptables -A FORWARD -i eth1 -p tcp --dport 23 -j ACCEPT
-iptables -A FORWARD -o eth1 -p tcp --sport 23 -j ACCEPT
-iptables -A FORWARD -i eth1 -p icmp -j ACCEPT
-iptables -A FORWARD -o eth1 -p icmp -j ACCEPT
+iptables -P OUTPUT  DROP
+
+#http
+iptables -A FORWARD -p tcp --sport 80 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+#iptables -A FORWARD -p tcp --sport 443 -j ACCEPT
+#iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
+
+#DNS
+iptables -A FORWARD -p udp --sport 53 -j ACCEPT
+iptables -A FORWARD -p udp --dport 53 -j ACCEPT
+
+#FTP
+iptables -A FORWARD -p tcp --sport 20 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 20 -j ACCEPT
+iptables -A FORWARD -p tcp --sport 21 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 21 -j ACCEPT
+
+#Telnet
+iptables -A FORWARD -p tcp --sport 23 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 23 -j ACCEPT
+
+#ICMP
+iptables -A FORWARD -p icmp -j ACCEPT
+
+#SSH
+iptables -A FORWARD -p tcp --sport 22 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
