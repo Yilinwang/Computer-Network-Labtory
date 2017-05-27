afaf40a7dda6b748d2f500dab9a65b706da1365f
chipyaya <b03902024@ntu.edu.tw>
2017-05-27 18:14:00 +0800

    get available wifi connections

--- FPJ/notifierwifi/index.js
+++ FPJ/notifierwifi/index.js
@@ -1,28 +1,49 @@
-/*
-var iwlist = require('wireless-tools/iwlist');
-
-iwlist.scan('wlp1s0', function(err, networks) {
-    console.log(networks);
-});
-
-iwlist.scan({ iface : 'wlp1s0', show_hidden : true }, function(err, networks) {
-    console.log(networks);
-});
-
-
-var ifconfig = require('wireless-tools/ifconfig');
+var wifi = require('node-wifi');
  
-ifconfig.status(function(err, status) {
-    console.log(status);
+// Initialize wifi module 
+// Absolutely necessary even to set interface to null 
+wifi.init({
+    iface : null // network interface, choose a random wifi interface if set to null 
 });
-*/
-
-const scanner = require('node-wifi-scanner');
-
-scanner.scan((err, networks) => {
+ 
+// Scan networks 
+wifi.scan(function(err, networks) {
     if (err) {
-        console.error(err);
-        return;
+        console.log(err);
+    } else {
+        console.log(networks);
+        /*
+        for (var i = 0; i < networks.length; i++){
+            console.log(networks[i].ssid, networks[i].frequency, networks[i].signal_level);
+        }
+
+		for (i in networks){
+			console.log(i);
+			for (key in networks[i]){
+				console.log( key + ": " + networks[i][key]);
+			}
+		}
+        */
+
+        // TODO: Filter SSID (only CSIE AP)
+
+        // TODO: Select max 3
+        // Problem wifi frequency (has a lot of channels)
     }
-      console.log(networks);
 });
+
+
+
+/*
+RETURNING DATA STRUCTURE:
+networks = [
+    {
+        ssid: '...',
+        mac: '...',
+        frequency: <number>, // in MHz
+        signal_level: <number>, // in dB
+        security: '...' // unfortunately the format still depends of the OS
+    },
+    ...
+];
+*/
