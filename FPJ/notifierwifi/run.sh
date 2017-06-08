a8554d04a89374a2eddae4201f3983ae919e4764
chipyaya <b03902024@ntu.edu.tw>
2017-06-08 20:37:23 +0800

    rm file

--- FPJ/notifierwifi/run.sh
+++
@@ -1,9 +0,0 @@
-for (( i=0; i <= 10; ++i ))
-do
-    node index.js | grep ^7\ >> a.txt
-    node index.js | grep ^13\ >> b.txt
-done
-awk '{sum+=$2}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}' a.txt
-awk '{sum+=$2}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}' b.txt
-rm -f a.txt
-rm -f b.txt
