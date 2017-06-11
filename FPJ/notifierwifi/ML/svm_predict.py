f3fa254e34c37f4eace3d3d6e8a823574aa9628f
smallfish0807 <smallfish0807@gmail.com>
2017-06-11 16:41:19 +0800

    add libsvm

--- FPJ/notifierwifi/ML/svm_predict.py
+++ FPJ/notifierwifi/ML/svm_predict.py
@@ -2,13 +2,20 @@ import sys
 sys.path.append('libsvm/python')
 from svmutil import *
 
-if len(sys.argv) != 5:
-	print('Usage :', sys.argv[0], 'model signal_7 signal_12 signal_13')
+if len(sys.argv) != 4:
+	print('Usage :', sys.argv[0], 'signal_7 signal_12 signal_13')
 	exit(1)
 
-x = [{i-1:float(sys.argv[i]) for i in range(2, 5)}]
+x = [{i:float(sys.argv[i]) for i in range(1, 4)}]
 y = [0]
 
-m = svm_load_model(sys.argv[1])
+# predict coordinate x
+m = svm_load_model('model_x')
 p_label, p_acc, p_val = svm_predict(y, x, m, '-q')
 print(p_label[0])
+
+# predict coordinate y
+m = svm_load_model('model_y')
+p_label, p_acc, p_val = svm_predict(y, x, m, '-q')
+print(p_label[0])
+
