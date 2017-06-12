a9386ae0c52944096f4b565869fcfd3fc9dbad3d
smallfish0807 <b03902022@ntu.edu.tw>
2017-06-12 14:26:35 +0800

    add validation in svc_train.py

--- FPJ/notifierwifi/ML/svc_train.py
+++ FPJ/notifierwifi/ML/svc_train.py
@@ -14,18 +14,26 @@ def cases(x, y):
 		else:
 			return 3
 
+
 with open('train_data', 'r') as f:
 	input_data = [[float(x) for x in line.lstrip().rstrip().split(' ')] for line in f]
 input_data = np.array(input_data)
 
-
 X = input_data[:, [2, 3, 4]]
 Y = np.array([cases(line[0], line[1]) for line in input_data])
-#Y = Y.reshape(-1, 1)
 
 model = SVC()
 model.fit(X, Y)
-print('mean classification error:', np.mean([1 if model.predict(X)[i] != Y[i] else 0 for i in range(len(Y))]))
+print('E_in:', np.mean([1 if model.predict(X)[i] != Y[i] else 0 for i in range(len(Y))]))
+
+with open('validate_data', 'r') as f:
+	input_data = [[float(x) for x in line.lstrip().rstrip().split(' ')] for line in f]
+input_data = np.array(input_data)
+
+X = input_data[:, [2, 3, 4]]
+Y = np.array([cases(line[0], line[1]) for line in input_data])
+
+print('E_val:', np.mean([1 if model.predict(X)[i] != Y[i] else 0 for i in range(len(Y))]))
 
 pickle.dump(model, open('svc_model', 'wb'))
 
